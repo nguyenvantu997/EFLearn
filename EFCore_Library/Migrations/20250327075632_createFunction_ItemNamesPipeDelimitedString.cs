cd9519cb3915a665b98561e9e319a619d6fc4da7
Tu Nguyen <nvt977@gmail.com>
2025-03-27 15:00:31 +0700

    sagve

---
+++ EFCore_Library/Migrations/20250327075632_createFunction_ItemNamesPipeDelimitedString.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class createFunctionItemNamesPipeDelimitedString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.SqlResource("EFCore_Library.Scripts.Functions.ItemNamesPipeDelimitedString.ItemNamesPipeDelimitedString.v0.sql");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP FUNCTON IF EXISTS Dbo.ItemNamesPipeDelimitedString");
+        }
+    }
+}
---
+++ EFCore_Library/Scripts/Functions/ItemNamesPipeDelimitedString/ItemNamesPipeDelimitedString.v0.sql
@@ -0,0 +1,9 @@
+CREATE OR ALTER FUNCTION [dbo].[ItemNamesPipeDelimitedString] 
+(@IsActive BIT) 
+RETURNS VARCHAR (2500) 
+AS 
+BEGIN 
+	RETURN (SELECT STRING_AGG (Name, '|') 
+			FROM Items 
+			WHERE IsActive = @IsActive) 
+END
